49963574dc0729d1bbb9818a8db7e3b600ec28b0
Hugo Santos <desarrollo@hugosantos.com.mx>
2025-08-04 10:06:55 -0600

    dashboard, rutas y controller

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Home;
+use App\Http\Controllers\Dashboard;
 
 Route::get('/', [Home::class, 'index'])->name('home.index');
+
+Route::prefix('dashboard')->group(function () {
+    Route::get('/', [Dashboard::class, 'index'])->name('dashboard.index');
+
+    
+    Route::get('/posts', [Dashboard::class, 'posts'])->name('dashboard.posts');
+    Route::get('/posts/create', [Dashboard::class, 'createPost'])->name('dashboard.posts.create');
+    Route::post('/posts/store', [Dashboard::class, 'storePost'])->name('dashboard.posts.store');
+    Route::get('/posts/{id}/edit', [Dashboard::class, 'editPost'])->name('dashboard.posts.edit');
+    Route::put('/posts/{id}', [Dashboard::class, 'updatePost'])->name('dashboard.posts.update');
+    Route::delete('/posts/{id}', [Dashboard::class, 'deletePost'])->name('dashboard.posts.delete');
+    
+    Route::get('/settings', [Dashboard::class, 'settings'])->name('dashboard.settings');
+    Route::get('/profile', [Dashboard::class, 'profile'])->name('dashboard.profile');
+    Route::get('/notifications', [Dashboard::class, 'notifications'])->name('dashboard.notifications');
+});
