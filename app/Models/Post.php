ebffa3b23500c0777eb55a50e1f035962d4d41ae
Hugo Santos <desarrollo@hugosantos.com.mx>
2025-08-11 12:56:07 -0600

    nuevos modelos y migraciones

---
+++ app/Models/Post.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
+class Post extends Model
+{
+    use Sluggable;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'excerpt',
+        'content',
+        'thumbnail',
+        'type',
+        'user_id',
+        'views',
+    ];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array<string, mixed>
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+}
