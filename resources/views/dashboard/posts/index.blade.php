caf772bae1f8fc703875dcc19f650f986f359700
Hugo Santos <desarrollo@hugosantos.com.mx>
2025-08-14 17:28:05 -0600

    ajuste para diferir imagenes del blog y de proyectos

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="row">
     <div class="col-12 d-flex justify-content-between align-items-center mb-4">
-        <h1 class="h3 m-0">Posts</h1>
+        <h1 class="h3 m-0">Publicaciones</h1>
         <a href="{{ route('dashboard.posts.create') }}" class="btn btn-primary"><i class="bi bi-plus-circle"></i> Crear Post</a>
     </div>
 
